4e9437b056ece36413d7b8aad301db0126f6fe5b
MariaPrikhozhaya <92399204+MariaPrikhozhaya@users.noreply.github.com>
2024-07-04 21:50:59 +0300

    Update styles.ts

--- Test Task/city_guide/src/components/linkToAccount/styles.ts
+++ Test Task/city_guide/src/components/linkToAccount/styles.ts
@@ -1,15 +1,22 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const SAvatar = styled.div`
   position: absolute;
-  bottom: 50px; 
+  display: inline-block;
+  bottom: 45px;
   left: 50%;
   transform: translateX(-50%);
-  display: inline-block;
 `;
 
 export const SButton = styled.button`
   background: none;
   border: none;
   cursor: pointer;
+  padding: 0;
+`;
+
+export const SAvatarMobile = styled.div`
+  text-align: center;
+  display: flex;
+  justify-content: center;
 `;
