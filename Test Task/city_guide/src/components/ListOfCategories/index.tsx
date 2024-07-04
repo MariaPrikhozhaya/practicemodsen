aa569033c6114a7715c4e0895572a4e05ed411d1
MariaPrikhozhaya <92399204+MariaPrikhozhaya@users.noreply.github.com>
2024-07-04 21:48:46 +0300

    Update index.tsx

--- Test Task/city_guide/src/components/ListOfCategories/index.tsx
+++ Test Task/city_guide/src/components/ListOfCategories/index.tsx
@@ -1,37 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { categories } from '../../constants/categories';
-import { SList, SLi, SAvatar, SBlock } from "./styles";
+import { SList, SLi, SAvatar, SBlock } from './styles';
 import { useAppDispatch } from '../../hooks/redux';
 import { setSelectedCategories } from '../../store/reducers/geoObjects';
 
 const CategoryList = () => {
-
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
   const [filterCategories, setFilterCategories] = useState([]);
 
   useEffect(() => {
-      setFilterCategories(categories);
+    setFilterCategories(categories);
   }, [categories]);
 
   const handleClick = (index: number) => {
     setFilterCategories(
-        filterCategories.map((el, i) =>
-            i === index ? { ...el, isSelected: !el.isSelected } : el
-        )
+      filterCategories.map((el, i) =>
+        i === index ? { ...el, isSelected: !el.isSelected } : el
+      )
     );
   };
 
   useEffect(() => {
-    console.log(setSelectedCategories(filterCategories.filter(el => el.isSelected)));
-    dispatch(setSelectedCategories(filterCategories.filter(el => el.isSelected)));
-}, [filterCategories])
+    console.log(
+      setSelectedCategories(filterCategories.filter((el) => el.isSelected))
+    );
+    dispatch(
+      setSelectedCategories(filterCategories.filter((el) => el.isSelected))
+    );
+  }, [filterCategories]);
 
   return (
     <SBlock>
       <SList>
         {filterCategories.map((category, index) => (
-          <SLi isSelected={category.isSelected} onClick={() => handleClick(index)}>
-            <SAvatar src={category.icon}/> {category.text}
+          <SLi
+            isSelected={category.isSelected}
+            onClick={() => handleClick(index)}
+          >
+            <SAvatar src={category.icon} /> {category.text}
           </SLi>
         ))}
       </SList>
