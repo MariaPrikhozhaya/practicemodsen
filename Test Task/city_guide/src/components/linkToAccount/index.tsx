12147d4c84d4bac443b8cce3dd31ff7e4fa0700e
MariaPrikhozhaya <92399204+MariaPrikhozhaya@users.noreply.github.com>
2024-07-04 21:50:23 +0300

    Update index.tsx

--- Test Task/city_guide/src/components/linkToAccount/index.tsx
+++ Test Task/city_guide/src/components/linkToAccount/index.tsx
@@ -1,34 +1,50 @@
-import React from "react";
+import React from 'react';
 import { Avatar } from '@mui/material';
-import { SAvatar, SButton } from "./styles";
-import { useAuth } from "../../hooks/useAuth";
-import { useAppDispatch } from "../../hooks/redux";
-import { removeUser } from "../../store/reducers/userSlice";
-import login from '@assets/login.png'
+import { SAvatar, SButton, SAvatarMobile } from './styles';
+import { useAuth } from '../../hooks/useAuth';
+import { useAppDispatch } from '../../hooks/redux';
+import { removeUser } from '../../store/reducers/userSlice';
+import login from '@assets/login.png';
+import { Desktop, Mobile } from '../../constants/version';
 
 const LinkAccount = () => {
-  
-  const {isAuth} = useAuth();
-    const dispatch = useAppDispatch();
+  const { isAuth } = useAuth();
+  const dispatch = useAppDispatch();
 
-    return (
-      isAuth ? (
-        <SAvatar>
-        <SButton
-          onClick={() => dispatch(removeUser())}>
-          <Avatar src="/broken-image.jpg"/>
-        </SButton>
-      </SAvatar>
-      ) : (
-        <SAvatar>
-          <a
-            href="/login"
-            role="button">
-            <img src={login}/>
-          </a>
-        </SAvatar>
-      )
-    )
+  return (
+    <>
+      <Desktop>
+        {isAuth ? (
+          <SAvatar>
+            <SButton onClick={() => dispatch(removeUser())}>
+              <Avatar src="/broken-image.jpg" />
+            </SButton>
+          </SAvatar>
+        ) : (
+          <SAvatar>
+            <a href="/login" role="button">
+              <Avatar src={login} />
+            </a>
+          </SAvatar>
+        )}
+      </Desktop>
+      <Mobile>
+        {isAuth ? (
+          <SAvatarMobile>
+            <SButton onClick={() => dispatch(removeUser())}>
+              <Avatar src="/broken-image.jpg" />
+            </SButton>
+          </SAvatarMobile>
+        ) : (
+          <SAvatarMobile>
+            <a href="/login" role="button">
+              <Avatar src={login} />
+            </a>
+          </SAvatarMobile>
+        )}
+      </Mobile>
+    </>
+  );
 };
 
 export default LinkAccount;
