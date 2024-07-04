370d84ced0574fc1ba481c4cc13b674878286f27
MariaPrikhozhaya <92399204+MariaPrikhozhaya@users.noreply.github.com>
2024-07-04 21:45:58 +0300

    Update styles.ts

--- Test Task/city_guide/src/components/ObjectInfo/styles.ts
+++ Test Task/city_guide/src/components/ObjectInfo/styles.ts
@@ -1,49 +1,48 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const SCard = styled.div`
-    position: absolute;
-    top: 20px;
-    right: 20px;
-    background-color: white;
-    padding: 10px;
-    border-radius: 6px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-    width: 320px;
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  background-color: white;
+  padding: 10px;
+  border-radius: 6px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  width: 320px;
 `;
 
 export const SA = styled.a`
-    display: block; 
-    wordBreak: break-all;
-    textOverflow: ellipsis;
-    whiteSpace: nowrap; 
-    overflow: hidden;
+  display: block;
+  wordbreak: break-all;
+  textoverflow: ellipsis;
+  whitespace: nowrap;
+  overflow: hidden;
 `;
 
-
 export const SButtonFav = styled.button`
-    padding: 7px 14px;
-    font-size: 14px;
-    background: #C75E5E;
-    border-radius: 6px;
-    border: none;
-    cursor: pointer;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-    width: 100%;
+  padding: 7px 14px;
+  font-size: 14px;
+  background: #c75e5e;
+  border-radius: 6px;
+  border: none;
+  cursor: pointer;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  width: 100%;
 `;
 
 export const SButtonRoute = styled.button`
-    padding: 7px 14px;
-    font-size: 14px;
-    background: #5E7BC7;
-    border-radius: 6px;
-    border: none;
-    cursor: pointer;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-    margin-top: 10px;
-    width: 100%;
+  padding: 7px 14px;
+  font-size: 14px;
+  background: #5e7bc7;
+  border-radius: 6px;
+  border: none;
+  cursor: pointer;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  width: 100%;
 `;
