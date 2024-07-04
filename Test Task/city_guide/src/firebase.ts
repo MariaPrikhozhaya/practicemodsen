203ebb4cf6c00f48522f174a1f8810b131620ada
MariaPrikhozhaya <92399204+MariaPrikhozhaya@users.noreply.github.com>
2024-07-04 22:04:43 +0300

    Update firebase.ts

--- Test Task/city_guide/src/firebase.ts
+++ Test Task/city_guide/src/firebase.ts
@@ -1,16 +1,15 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection } from "@firebase/firestore"
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection } from '@firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-6kZAJsUOD3Klls8RUI07HouFtrf6ttw",
-  authDomain: "mapreact-5dec3.firebaseapp.com",
-  projectId: "mapreact-5dec3",
-  storageBucket: "mapreact-5dec3.appspot.com",
-  messagingSenderId: "872030377063",
-  appId: "1:872030377063:web:ff09456828f7968b1b30c2"
+  apiKey: 'AIzaSyA-6kZAJsUOD3Klls8RUI07HouFtrf6ttw',
+  authDomain: 'mapreact-5dec3.firebaseapp.com',
+  projectId: 'mapreact-5dec3',
+  storageBucket: 'mapreact-5dec3.appspot.com',
+  messagingSenderId: '872030377063',
+  appId: '1:872030377063:web:ff09456828f7968b1b30c2',
 };
-
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const FavCollectionRef = collection(db, "favorites");
+export const FavCollectionRef = collection(db, 'favorites');
