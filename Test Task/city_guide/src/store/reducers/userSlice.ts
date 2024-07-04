9f577e16e27d1197ae4364402d244e05ff14b373
MariaPrikhozhaya <92399204+MariaPrikhozhaya@users.noreply.github.com>
2024-07-04 22:02:03 +0300

    Update userSlice.ts

--- Test Task/city_guide/src/store/reducers/userSlice.ts
+++ Test Task/city_guide/src/store/reducers/userSlice.ts
@@ -1,37 +1,34 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 interface User {
-    id: string;
-    email: string;
-    token: string;
+  id: string;
+  email: string;
+  token: string;
 }
 
 const initialState: User = {
-    id: null,
-    email: null,
-    token: null
+  id: null,
+  email: null,
+  token: null,
 };
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers: {
-        setUser(state, action) {
-            state.id = action.payload.id;
-            state.email = action.payload.email;
-            state.token = action.payload.token;
-        },
-        removeUser(state) {
-            state.id = null;
-            state.email = null;
-            state.token = null;
-        }
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.id = action.payload.id;
+      state.email = action.payload.email;
+      state.token = action.payload.token;
     },
-})
+    removeUser(state) {
+      state.id = null;
+      state.email = null;
+      state.token = null;
+    },
+  },
+});
 
-export const {
-    setUser,
-    removeUser
-} = userSlice.actions;
+export const { setUser, removeUser } = userSlice.actions;
 
 export default userSlice.reducer;
