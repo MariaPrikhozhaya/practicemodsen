d1dc007e07d1dcfc01052665fcfce1c421a9dfff
MariaPrikhozhaya <92399204+MariaPrikhozhaya@users.noreply.github.com>
2024-07-04 22:00:46 +0300

    Update Register.tsx

--- Test Task/city_guide/src/pages/Register.tsx
+++ Test Task/city_guide/src/pages/Register.tsx
@@ -1,11 +1,8 @@
-import React from "react";
-import RegisterForm from "../components/RegisterForm";
-
+import React from 'react';
+import RegisterForm from '../components/RegisterForm';
 
 const Register = () => {
-	return (
-		<RegisterForm/>
-	)
-}
+  return <RegisterForm />;
+};
 
-export default Register
+export default Register;
