0789afc56424ba597ef68b6a98e0bae1244d9387
MariaPrikhozhaya <92399204+MariaPrikhozhaya@users.noreply.github.com>
2024-07-04 20:24:26 +0300

    Update styles.ts

--- Test Task/city_guide/src/components/Map/styles.ts
+++ Test Task/city_guide/src/components/Map/styles.ts
@@ -1,13 +1,13 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const SMdClose = styled.div`
-    position: absolute;
-    top: 20px;
-    background: #fff;
-    padding: 10px;
-    border-radius: 5px;
-    box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-    cursor: pointer;
-    border: none;
-    right: 370px; 
+  position: absolute;
+  top: 20px;
+  background: #fff;
+  padding: 10px;
+  border-radius: 5px;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  border: none;
+  right: 370px;
 `;
