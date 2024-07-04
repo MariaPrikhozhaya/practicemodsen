5caa852a5b1ebf80990c652d15fa8bad4d2d93f7
MariaPrikhozhaya <92399204+MariaPrikhozhaya@users.noreply.github.com>
2024-07-04 21:46:35 +0300

    Update index.tsx

--- Test Task/city_guide/src/components/RouteInfo/index.tsx
+++ Test Task/city_guide/src/components/RouteInfo/index.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import {
-    SCard, SDiv, SDistance, STime, STextValue, STextName
-} from "./styles";
+import { SCard, SDiv, SDistance, STime, STextValue, STextName } from './styles';
 import { useAppSelector } from '../../hooks/redux';
 
 const RouteInfo = () => {
+  const geoObjects = useAppSelector((state) => state.geoObjectsReducer);
 
-    const geoObjects = useAppSelector(state => state.geoObjectsReducer);
-
-    return (
-        <SCard>
-            <SDiv>
-                <SDistance>
-                    <STextValue>{geoObjects.route.length} km</STextValue>
-                    <STextName>дистанция</STextName>
-                </SDistance>
-                <STime>
-                    <STextValue>{geoObjects.route.duration} мин</STextValue>
-                    <STextName>примерное время</STextName>
-                </STime>
-            </SDiv>
-        </SCard>
-    );
-}
+  return (
+    <SCard>
+      <SDiv>
+        <SDistance>
+          <STextValue>{geoObjects.route.length} km</STextValue>
+          <STextName>дистанция</STextName>
+        </SDistance>
+        <STime>
+          <STextValue>{geoObjects.route.duration} мин</STextValue>
+          <STextName>примерное время</STextName>
+        </STime>
+      </SDiv>
+    </SCard>
+  );
+};
 
 export default RouteInfo;
